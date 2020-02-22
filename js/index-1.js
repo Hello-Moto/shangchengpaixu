1077643dd987fc8b354711ec3b0de859dd388989
Hello-Moto <you@example.com>
2020-02-22 15:08:11 +0800

    代码优化

--- js/index-1.js
+++ js/index-1.js
@@ -1,81 +1,69 @@
-let list = document.getElementById("list");
-let headerBox = document.getElementById("header"),
-    linkList = headerBox.getElementsByTagName("a"),
-    productList = list.getElementsByTagName("li");
-!function(){
-    let data = null;
-    let xhr = new XMLHttpRequest();
-    xhr.open("get","json/product.txt",false);
-    xhr.onreadystatechange = ()=>{
-        if(xhr.readyState === 4 && xhr.status === 200){
-            data = xhr.responseText;
-            data = JSON.parse(data);
+let productRender = (function(){
+    let data = null,
+        list = document.getElementById("list"),
+        headerBox = document.getElementById("header"),
+        linkList = headerBox.getElementsByTagName("a"),
+        productList = null;
+    let getData = function(){//获取数据
+        let xhr = new XMLHttpRequest();
+        xhr.open('get','json/product.txt',false);
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4 && xhr.status === 200){
+                data = JSON.parse(xhr.responseText);
+            }
         }
-    };
-    xhr.send(null);
-    let str = ``;
-    for(var i = 0; i < data.length; i++){
-        let {title,img,price,time,hot} = data[i];
-        str += `<li data-price = "${price}" data-time = "${time}" data-hot = "${hot}" ><a href="javascript:;">
+        xhr.send(null);
+    }
+    let bindHtml = function(){//完成数据绑定
+        let str = ``;
+        data.forEach(function({title,img,price,time,hot},index){
+            str += `<li data-price = "${price}" data-time = "${time}" data-hot = "${hot}" ><a href="javascript:;">
             <img src="${img}" alt="">
             <p>${title}</p>
             <span>￥${price}</span><br/>
             <span>${time}</span><br/>
             <span>热度：${hot}</span><br/>
-        </a></li>`;
+            </a></li>`;
+        })
+        list.innerHTML = str;
+        productList = list.querySelectorAll("li");
     }
-    list.innerHTML = str;
-}();
-!function(){
-    let sortList = function(){//按照价格升序排列
-        let productArr = [].slice.call(productList);//把类数组转化为数组
-        productArr.sort((a,b)=>{
-            let aInn,bInn;
-            // switch(this.index){//第一种方法
-            //     case 0:
-            //         aInn = a.getAttribute('data-time').replace(/-/g,'');
-            //         bInn = b.getAttribute('data-time').replace(/-/g,'');
-            //         break;
-            //     case 1:
-            //         aInn = a.getAttribute("data-price");
-            //         bInn = b.getAttribute("data-price");
-            //         break;
-            //     case 2:
-            //         aInn = a.getAttribute("data-hot");
-            //         bInn = b.getAttribute("data-hot");
-            //         break;
-            // };
-
-            //第二种方法
-            let arr = ['data-time','data-price','data-hot'];
-            aInn = a.getAttribute(arr[this.index]);
-            bInn = b.getAttribute(arr[this.index]);
-            if(this.index === 0){
-                aInn = aInn.replace(/-/g,'');
-                bInn = bInn.replace(/-/g,'');
+    let bindClick = function(){//绑定点击事件
+        [].forEach.call(linkList,(item,index)=>{//循环三次执行三个方法每一次执行都会形成一个闭包，每一个闭包中都保存了当前这个a标签对应索引
+            item.flag = -1;
+            item.onclick = function(){
+                [].forEach.call(linkList,(item,index)=>{
+                    if(item !== this){
+                        item.flag = -1;
+                    }
+                })
+                this.flag *= -1;
+                //根据点击列的不同给productList进行排序
+                let arr = ['data-time','data-price','data-hot'];
+                productList = [].slice.call(productList);
+                productList.sort((a,b)=>{
+                    // console.log(productList)
+                    let aInn = a.getAttribute(arr[index]),
+                        bInn = b.getAttribute(arr[index]);
+                    if(index === 0){//点击日期把字符串转换为数字，去掉中间的‘-’
+                        aInn = aInn.replace(/-/g,'');
+                        bInn = bInn.replace(/-/g,'');
+                    }
+                    return (aInn - bInn)*this.flag;
+                });
+                //按照最新顺序依次添加到容器中
+                productList.forEach((curLi,index)=>{
+                    list.appendChild(curLi);
+                })
             }
-            return (aInn - bInn)*this.flag;
-        })//数组已经排好序
-        //按照数组排好的序，把li重新增加到页面中
-        for(let i = 0; i < productArr.length; i++){
-            let curLi = productArr[i];
-            list.appendChild(curLi);
-        }
-    };
-    //循环绑定点击事件
-    for(let i = 0; i < linkList.length; i++){
-        let curList = linkList[i];
-        curList.flag = -1;
-        curList.index = i;
-        curList.onclick = function(){
-            for(var j = 0; j < linkList.length; j++){
-                let item = linkList[i];
-                if(item !== this){
-                    item.flag = -1;
-                }
-            }
-            this.flag *= -1;
-            sortList.call(this);
+        })
+    }
+    return {
+        init: function(){
+            getData();
+            bindHtml();
+            bindClick();
         }
     }
-}();
+})();
+productRender.init();
